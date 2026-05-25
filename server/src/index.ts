920bac1ce57f6bf564eba708d866a7e579587d8f
kashish <kgupta7827@gmail.com>
2026-05-25 17:44:23 +0530

    minor changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,13 +4,20 @@ const server = new WebSocketServer({
     port:8081
 })
 
+let totalConnection = 0;
+
 server.on('connection',(socket)=>{
-    console.log("Client connected")
+    console.log("Client connected",totalConnection++)
+
 
     socket.on('message',(message)=>{
-        console.log(`Received ${message}`)
+        console.log(`Received message from Client ${totalConnection} ${message}`)
         socket.send(`Server  : ${message}`)
     });
+
+    if (totalConnection >3) {
+        socket.close();
+    }
 
     socket.on('close',()=>{
         console.log('Clinet disconnected')
